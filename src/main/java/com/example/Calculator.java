257202d39ffe31cdf80a000f28a8569af7c19d6b
Eli Newland <enewland22@gmail.com>
2025-05-06 12:49:55 -0500

    injected fault in Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     }
 
     public int multiply(int a, int b) {
-        return a * b;
+        return a * b - 1;
     }
 
     public double divide(double a, double b) {
